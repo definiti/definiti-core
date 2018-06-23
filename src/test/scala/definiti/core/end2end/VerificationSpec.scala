c8af05153dc8cc5a637de0f4ac11e18b3f297c50
Gaëtan Rizio <gaetan@rizio.fr>
2018-06-23 09:51:16 +0200

    [#60] Parameter type can be implicitly extracted from its name - verifications
    To simplify reading and writing, the parameter type definition can be implicit.
    It will be extracted from the parameter name by capitalizing the name.
    
    This commit focus on verification parameters.
    
    Previous commit already did the work so we just add tests to ensure the process.
    
    This commit does the following:
    
    * Add two tests to control the new behavior

--- src/test/scala/definiti/core/end2end/VerificationSpec.scala
+++ src/test/scala/definiti/core/end2end/VerificationSpec.scala
@@ -1,9 +1,10 @@
 package definiti.core.end2end
 
 import definiti.common.ast._
-import definiti.common.program.Ok
+import definiti.common.program.{Ko, Ok}
 import definiti.common.tests.LocationPath
 import definiti.common.utils.ASTUtils._
+import definiti.common.validation.AlertLocation
 import definiti.core.ProgramResultMatchers._
 
 class VerificationSpec extends EndToEndSpec {
@@ -13,6 +14,18 @@ class VerificationSpec extends EndToEndSpec {
     val expected = Ok(validNonEmptyList)
     val output = processFile("verification.NonEmptyList")
     output should beResult[Root](expected)
+  }
+
+  it should "generate the AST for a valid verification without a type name" in {
+    val expected = Ok(validNonEmptyString)
+    val output = processFile("verification.NonEmptyString")
+    output should beResult[Root](expected)
+  }
+
+  it should "refuse to generate the AST for a verification without a type name and an invalid parameter name" in {
+    val expected = Ko[Root](nonEmptyUnknown)
+    val output = processFile("verification.NonEmptyUnknown")
+    output should beResult(expected)
   }
 }
 
@@ -40,6 +53,40 @@ object VerificationSpec {
       ),
       comment = None,
       location = nonEmptyListLocation(1, 1, 6, 2)
+    )
+  )
+
+  val nonEmptyStringSrc = "src/test/resources/samples/verification/NonEmptyString.def"
+  val nonEmptyStringLocation = LocationPath(nonEmptyStringSrc)
+  val validNonEmptyString = root(
+    Verification(
+      name = "NonEmptyString",
+      fullName = "NonEmptyString",
+      parameters = Seq.empty,
+      message = LiteralMessage("The string should not be empty", nonEmptyStringLocation(2, 3, 35)),
+      function = DefinedFunction(
+        parameters = Seq(ParameterDefinition("string", TypeReference("String"), nonEmptyStringLocation(3, 4, 10))),
+        body = MethodCall(
+          expression = Reference("string", TypeReference("String"), nonEmptyStringLocation(4, 5, 11)),
+          method = "nonEmpty",
+          parameters = Seq.empty,
+          generics = Seq.empty,
+          returnType = TypeReference("Boolean"),
+          location = nonEmptyStringLocation(4, 5, 22)
+        ),
+        genericTypes = Seq.empty,
+        location = nonEmptyStringLocation(3, 3, 5, 4)
+      ),
+      comment = None,
+      location = nonEmptyStringLocation(1, 1, 6, 2)
+    )
+  )
+
+  val nonEmptyUnknownSrc = "src/test/resources/samples/verification/NonEmptyUnknown.def"
+  val nonEmptyUnknown = Seq(
+    AlertLocation(
+      "Class Unknown not found when trying to determine the type of the expression",
+      Location(nonEmptyUnknownSrc, 4, 5, 4, 12)
     )
   )
 }
